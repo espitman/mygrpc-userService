496fc8d675ec8ab98106422adb7b4929b4b15206
s.heidari <s.heidari@jabama.com>
2022-04-05 02:43:07 +0430

    change id type

--- .history/user/user_20220404235734.go
+++ .history/user/user_20220404235734.go
@@ -10,5 +10,5 @@ type Server struct {
 
 func (s *Server) GetUser(ctx context.Context, in *UserRequest) (*UserResponse, error) {
 	log.Println("Receive message body from client:")
-	return &UserResponse{Id: in.Id, Fname: "Saeed", Lname: "Heidari"}, nil
+	return &UserResponse{Id: 1, Fname: "Saeed", Lname: "Heidari"}, nil
 }
